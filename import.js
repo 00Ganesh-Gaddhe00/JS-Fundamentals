60d9f9ffa37726fb5c8c1610166aab79ea6e659b
Ganesh <00mr.lg00@gmail.com>
2024-06-26 11:29:40 +0530

    NOTES

---
+++ import.js
@@ -0,0 +1,6 @@
+const sum = require("./index")
+
+
+console.log(sum())
+
+console.log(module)
---
+++ index.js
@@ -0,0 +1,15 @@
+let a = 10
+let b = 20
+var c = 30
+
+function sum(){
+    console.log(a+b+c)
+}
+
+
+module.exports = sum 
+
+console.log(module)
+
+
+
