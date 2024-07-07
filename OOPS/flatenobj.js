3f4f4f31ab0e4b4b2a088a3b264bf1f3ada714c4
Ganesh <00mr.lg00@gmail.com>
2024-07-07 17:36:10 +0530

    flatening object

---
+++ OOPS/flatenobj.js
@@ -0,0 +1,26 @@
+
+const obj = {
+    name: "garath",
+    lastname:"curt",
+    age:30,
+
+}
+
+// console.log(obj)
+
+
+// for(let key in obj){
+//     let val = key
+//     console.log(val)
+// }
+
+// for(let [key,value] of Object.entries(obj)){
+//     console.log(key +':'+ value)
+// }
+
+
+// console.log(typeof null)
+console.log(val3)
+let val1 = 9
+const val2 = 10
+var val3 = 11
